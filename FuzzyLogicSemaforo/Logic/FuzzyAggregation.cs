8d9fca80ae2d52386714b19753e0256dfda8de41
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-04-08 16:55:22 -0500

    Logic Folder

---
+++ FuzzyLogicSemaforo/Logic/FuzzyAggregation.cs
@@ -0,0 +1,35 @@
+﻿using FuzzyLogicSemaforo.Fuzzificación;
+using System;
+using System.Collections.Generic;
+
+namespace FuzzyLogicSemaforo.Logic
+{
+    public static class FuzzyAggregation
+    {
+        public static Dictionary<double, double> AggregateOutput(
+            List<(FuzzyRule rule, double activation)> activeRules,
+            double start, double end, double step)
+        {
+            var maxAgregado = new Dictionary<double, double>();
+            for (double x = start; x <= end; x += step)
+            {
+                maxAgregado[x] = 0.0;
+            }
+            foreach (var (rule, activation) in activeRules)
+            {
+                FuzzyLabel salida = rule.Consequent;
+                foreach (var kvp in maxAgregado)
+                {
+                    double xValue = kvp.Key;
+                    double membershipSalida = salida.GetMembership(xValue);
+                    double recortado = Math.Min(membershipSalida, activation);
+                    if (recortado > maxAgregado[xValue])
+                    {
+                        maxAgregado[xValue] = recortado;
+                    }
+                }
+            }
+            return maxAgregado;
+        }
+    }
+}
