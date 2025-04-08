5b7196c8cf804659d9a6e1d2029ad876953eea10
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-04-08 16:55:32 -0500

    Fuzzy folder

---
+++ FuzzyLogicSemaforo/Fuzzificación/FuzzyLabel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyLogicSemaforo.Fuzzificación
+{
+    public class FuzzyLabel
+    {
+        public string VariableName { get; set; }
+        public string LabelName { get; set; }
+        public Func<double, double> MembershipFunction { get; set; }
+        public FuzzyLabel(string variable, string label, Func<double, double> membership)
+        {
+            VariableName = variable;
+            LabelName = label;
+            MembershipFunction = membership;
+        }
+        public double GetMembership(double x)
+        {
+            return MembershipFunction(x);
+        }
+    }
+}
---
+++ FuzzyLogicSemaforo/Fuzzificación/FuzzyRule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyLogicSemaforo.Fuzzificación
+{
+    public class FuzzyRule
+    {
+        public List<FuzzyLabel> Antecedents { get; set; }
+        public FuzzyLabel Consequent { get; set; }
+        public string Operator { get; set; } = "AND";
+        public FuzzyRule(List<FuzzyLabel> antecedents, FuzzyLabel consequent, string op = "AND")
+        {
+            Antecedents = antecedents;
+            Consequent = consequent;
+            Operator = op;
+        }
+        public double GetActivation(Dictionary<string, double> crispInputs)
+        {
+            double activation;
+            if (Operator == "AND")
+            {
+                // Tomamos el mínimo de todas las membresías
+                activation = double.MaxValue;
+                foreach (var antecedent in Antecedents)
+                {
+                    double valor = antecedent.GetMembership(crispInputs[antecedent.VariableName]);
+                    if (valor < activation)
+                        activation = valor;
+                }
+            }
+            else if (Operator == "OR")
+            {
+                activation = 0.0;
+                foreach (var antecedent in Antecedents)
+                {
+                    double valor = antecedent.GetMembership(crispInputs[antecedent.VariableName]);
+                    if (valor > activation)
+                        activation = valor;
+                }
+            }
+            else
+            {
+                activation = 0;
+            }
+            return activation;
+        }
+    }
+}
