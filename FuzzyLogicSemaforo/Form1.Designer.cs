c948b3c64514cc782dc4d41a5ddfab9ee94c1d61
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-04-08 16:15:01 -0500

    form designer

--- FuzzyLogicSemaforo/Form1.Designer.cs
+++ FuzzyLogicSemaforo/Form1.Designer.cs
@@ -118,6 +118,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
             ClientSize = new Size(1283, 707);
             Controls.Add(nudHora);
             Controls.Add(lblResultado);
